be60ca05af479a4575b50710583b9c049f903dc1
jainbhavuk <bhavukspeaks@gmail.com>
2023-08-05 01:53:16 +0530

    [EMP-APP] : Add get mapping for getting list of all employees

--- src/main/java/com/jainbhavuk/EmployeeCRUDApp/controllers/EmployeeController.java
+++ src/main/java/com/jainbhavuk/EmployeeCRUDApp/controllers/EmployeeController.java
@@ -3,10 +3,11 @@ package com.jainbhavuk.EmployeeCRUDApp.controllers;
 import com.jainbhavuk.EmployeeCRUDApp.entities.Employee;
 import com.jainbhavuk.EmployeeCRUDApp.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,8 +16,13 @@ public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @PostMapping("/create-employee")
-    private Employee createEmployee(@RequestBody  Employee employee){
+    @PostMapping("/employee")
+    private Employee createNewEmployee(@RequestBody  Employee employee){
        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employee")
+    private ResponseEntity<List<Employee>>  getAllEmployees(){
+        return new ResponseEntity<List<Employee>>(employeeRepository.findAll(), HttpStatus.OK);
     }
 }
