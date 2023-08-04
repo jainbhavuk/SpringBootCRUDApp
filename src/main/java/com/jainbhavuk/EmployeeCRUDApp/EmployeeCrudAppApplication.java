2ea194a22e55b51b099eb5c35798bf715fbc85d4
jainbhavuk <bhavukspeaks@gmail.com>
2023-08-04 22:27:54 +0530

    [EMP-APP] : initialize project

---
+++ src/main/java/com/jainbhavuk/EmployeeCRUDApp/EmployeeCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.jainbhavuk.EmployeeCRUDApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jainbhavuk/EmployeeCRUDApp/EmployeeCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jainbhavuk.EmployeeCRUDApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
